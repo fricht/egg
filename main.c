dbf3cce7066bbf638a5bb9f3d81530b9c3ab5bec
Fricht <fricht@github.com>
2025-08-12 16:27:31 +0200

    prepared for more modularity

--- main.c
+++ main.c
@@ -13,39 +13,38 @@ typedef struct {
 } Point;
 typedef Point Vec; // alias
 
-// L-shape
-#define NPTS 6
-Point PTS[NPTS] = {
-    {0, 7},
-    {0, 0},
-    {7, 0},
-    {7, 1},
-    {1, 1},
-    {1, 7}
-};
+typedef struct {
+    uint npts;
+    Point* shape;
+} Polygon;
 
-// G-shape (seems balanced)
-// #define NPTS 18
+// L-shape
+// #define NPTS 6
 // Point PTS[NPTS] = {
-//     {4, 0},
-//     {1, 0},
-//     {0, 1},
-//     {0, 6},
-//     {1, 7},
-//     {3, 7},
-//     {4, 6},
-//     {4, 5},
-//     {5, 5},
-//     {5, 4},
-//     {4, 4},
-//     {3, 4},
-//     {3, 5},
-//     {2, 6},
-//     {1, 5},
-//     {1, 2},
-//     {2, 1},
-//     {4, 1}
+//     {0, 7},
+//     {0, 0},
+//     {7, 0},
+//     {7, 1},
+//     {1, 1},
+//     {1, 7}
 // };
+
+// E
+#define NPTS 12
+Point PTS[NPTS] = {
+    {0, 9},
+    {0, 0},
+    {6, 0},
+    {6, 1},
+    {1, 1},
+    {1, 4},
+    {5, 4},
+    {5, 5},
+    {1, 5},
+    {1, 8},
+    {6, 8},
+    {6, 9}
+};
 
 double area(Point** points, uint length) {
     /*
@@ -118,34 +117,35 @@ int sorting_y(const void* p1, const void* p2) {
 
 double split_figure(
     Point* center, Vec* normal, // [pointer] [pointer] line definition
+    Polygon* polygon, // [pointer] the polygon (as in the name)
     Point* crossings, // [array] intersections holder (given, to stay in scope)
     Point** computed_shape, // [array [pointer]] active points (forming the shape)
     uint* shape_size // [pointer] nomber of points in computed_shape
 ) {
     uint crossings_counter = 0;
     *shape_size = 0;
-    bool was_active = is_point_active(center, normal, &PTS[0]);
+    bool was_active = is_point_active(center, normal, &polygon->shape[0]);
     if (was_active) {
-        computed_shape[*shape_size] = &PTS[0];
+        computed_shape[*shape_size] = &polygon->shape[0];
         (*shape_size)++;
     }
-    for (uint c = 1; c < NPTS; c++ /* haha what a joke XD */ ) {
-        bool is_active = is_point_active(center, normal, &PTS[c]);
+    for (uint c = 1; c < polygon->npts; c++ /* haha what a joke XD */ ) {
+        bool is_active = is_point_active(center, normal, &polygon->shape[c]);
         if (was_active != is_active) {
-            crossings[crossings_counter] = line_segment_intersection(center, normal, &PTS[c - 1], &PTS[c]);
+            crossings[crossings_counter] = line_segment_intersection(center, normal, &polygon->shape[c - 1], &polygon->shape[c]);
             computed_shape[*shape_size] = &crossings[crossings_counter];
             (*shape_size)++;
             crossings_counter++;
         }
         if (is_active) {
-            computed_shape[*shape_size] = &PTS[c];
+            computed_shape[*shape_size] = &polygon->shape[c];
             (*shape_size)++;
         }
         was_active = is_active;
     }
-    bool is_active = is_point_active(center, normal, &PTS[0]);
+    bool is_active = is_point_active(center, normal, &polygon->shape[0]);
     if (was_active != is_active) {
-        crossings[crossings_counter] = line_segment_intersection(center, normal, &PTS[NPTS - 1], &PTS[0]);
+        crossings[crossings_counter] = line_segment_intersection(center, normal, &polygon->shape[polygon->npts - 1], &polygon->shape[0]);
         computed_shape[*shape_size] = &crossings[crossings_counter];
         (*shape_size)++;
         crossings_counter++;
@@ -170,6 +170,7 @@ double split_figure(
 }
 
 Point compute_c_given_n(
+    Polygon* polygon, // polygon
     Vec* n, double* learn_rate, // actual data, vector expected normalized
     Point* mean_point, double* half_area, // cached data
     Point** shape, Point* crossings, uint* shape_size // reuse to avoid re-alloc
@@ -184,7 +185,7 @@ Point compute_c_given_n(
             new_c.x += n->x * movment;
             new_c.y += n->y * movment;
         }
-        last_cross_section = split_figure(&new_c, n, crossings, shape, shape_size);
+        last_cross_section = split_figure(&new_c, n, polygon, crossings, shape, shape_size);
         double new_area = area(shape, *shape_size);
         double new_err = *half_area - new_area;
         if (fabs(new_err) >= fabs(err)) {
@@ -196,21 +197,21 @@ Point compute_c_given_n(
     return c;
 }
 
-void you_spin_me_right_round(Point* Cs, Vec* Ns, uint samples, double learn_rate) {
+void you_spin_me_right_round(Polygon* polygon, Point* Cs, Vec* Ns, uint samples, double learn_rate) {
     // compute cache data
     double Sx = 0;
     double Sy = 0;
-    Point* all_area[NPTS];
-    for (uint i = 0; i < NPTS; i++) {
-        Sx += PTS[i].x;
-        Sy += PTS[i].y;
-        all_area[i] = &PTS[i];
+    Point* all_area[polygon->npts];
+    for (uint i = 0; i < polygon->npts; i++) {
+        Sx += polygon->shape[i].x;
+        Sy += polygon->shape[i].y;
+        all_area[i] = &polygon->shape[i];
     }
-    Point mean_point = {Sx / NPTS, Sy / NPTS};
-    double half_area = area(all_area, NPTS) / 2;
+    Point mean_point = {Sx / polygon->npts, Sy / polygon->npts};
+    double half_area = area(all_area, polygon->npts) / 2;
     // buffers & things
-    uint worst_case_shape = (uint)(NPTS + NPTS / 2);
-    uint worst_case_crossings = NPTS;
+    uint worst_case_shape = (uint)(polygon->npts + polygon->npts / 2);
+    uint worst_case_crossings = polygon->npts;
     Point* shape[worst_case_shape];
     Point crossings[worst_case_crossings];
     uint shape_size;
@@ -218,7 +219,7 @@ void you_spin_me_right_round(Point* Cs, Vec* Ns, uint samples, double learn_rate
     double angle = 0;
     for (uint i = 0; i < samples; i++) {
         Vec normal = {cos(angle), sin(angle)};
-        Point c = compute_c_given_n(&normal, &learn_rate, &mean_point, &half_area, shape, crossings, &shape_size);
+        Point c = compute_c_given_n(polygon, &normal, &learn_rate, &mean_point, &half_area, shape, crossings, &shape_size);
         Cs[i] = c;
         Ns[i] = normal;
         angle += delta_angle;
@@ -232,22 +233,22 @@ void compute_enveloppe(Point* Cs, Vec* Ns, uint samples, Point* enveloppe) {
     }
 }
 
-void create_svg(const char* filename, Point* enveloppe, uint envlp_size, bool show_envlp_points, bool show_envlp_lines) {
+void create_svg(const char* filename, Polygon* polygon, Point* enveloppe, uint envlp_size, bool show_envlp_points, bool show_envlp_lines) {
     FILE* svg_file = fopen(filename, "w");
     if (!svg_file) {
         printf("fopen '%s' failed", filename);
         return;
     }
     // compute canvas boundaries
-    double minX = PTS[0].x;
-    double maxX = PTS[0].x;
-    double minY = PTS[0].y;
-    double maxY = PTS[0].y;
-    for (uint i = 1; i < NPTS; i++) {
-        minX = (PTS[i].x < minX) ? PTS[i].x : minX;
-        maxX = (PTS[i].x > maxX) ? PTS[i].x : maxX;
-        minY = (PTS[i].y < minY) ? PTS[i].y : minY;
-        maxY = (PTS[i].y > maxY) ? PTS[i].y : maxY;
+    double minX = polygon->shape[0].x;
+    double maxX = polygon->shape[0].x;
+    double minY = polygon->shape[0].y;
+    double maxY = polygon->shape[0].y;
+    for (uint i = 1; i < polygon->npts; i++) {
+        minX = (polygon->shape[i].x < minX) ? polygon->shape[i].x : minX;
+        maxX = (polygon->shape[i].x > maxX) ? polygon->shape[i].x : maxX;
+        minY = (polygon->shape[i].y < minY) ? polygon->shape[i].y : minY;
+        maxY = (polygon->shape[i].y > maxY) ? polygon->shape[i].y : maxY;
     }
     // 10% margin
     double marginX = 0.1 * (maxX - minX);
@@ -265,12 +266,12 @@ void create_svg(const char* filename, Point* enveloppe, uint envlp_size, bool sh
     );
     double stroke_width = 0.04; // TODO : dynamically compute that
     // draw shape
-    for (uint i = 0; i < NPTS; i++) {
-        uint j = (i + 1) % NPTS;
+    for (uint i = 0; i < polygon->npts; i++) {
+        uint j = (i + 1) % polygon->npts;
         fprintf(
             svg_file,
             "<line x1=\"%lf\" y1=\"%lf\" x2=\"%lf\" y2=\"%lf\" stroke=\"black\" stroke-width=\"%lf\"/>\n",
-            PTS[i].x, PTS[i].y, PTS[j].x, PTS[j].y, stroke_width
+            polygon->shape[i].x, polygon->shape[i].y, polygon->shape[j].x, polygon->shape[j].y, stroke_width
         );
     }
     // draw enveloppe
@@ -301,8 +302,9 @@ int main() {
     Point Cs[samples];
     Vec Ns[samples];
     double learn_rate = 0.01;
-    you_spin_me_right_round(Cs, Ns, samples, learn_rate);
+    Polygon polygon = {NPTS, PTS};
+    you_spin_me_right_round(&polygon, Cs, Ns, samples, learn_rate);
     Point enveloppe[samples];
     compute_enveloppe(Cs, Ns, samples, enveloppe);
-    create_svg("out.svg", enveloppe, samples, false, true);
+    create_svg("out.svg", &polygon, enveloppe, samples, true, true);
 }
